e53b307a09721de2f16162577b3c64f0e8af7e6c
Jordan Mynes <mynesjordan@gmail.com>
2018-04-12 18:46:55 -0500

    Working wifi slider for servo, button for led

---
+++ README2.md
@@ -0,0 +1,12 @@
+SWITCHING TO ARDUINO MKR1000
+WIFI, IT'S GOT GAME
+
+Boards manager:
+install arduino samd boards for mkr1000
+
+Library manager:
+install wifi101
+
+Hotspot from mobile
+MANUALLY Installed library for blynk and compiled arduino ide not 
+from repo!
---
+++ arduino/blynk/blynk.ino
@@ -0,0 +1,81 @@
+/*************************************************************
+  Download latest Blynk library here:
+    https://github.com/blynkkk/blynk-library/releases/latest
+
+  Blynk is a platform with iOS and Android apps to control
+  Arduino, Raspberry Pi and the likes over the Internet.
+  You can easily build graphic interfaces for all your
+  projects by simply dragging and dropping widgets.
+
+    Downloads, docs, tutorials: http://www.blynk.cc
+    Sketch generator:           http://examples.blynk.cc
+    Blynk community:            http://community.blynk.cc
+    Follow us:                  http://www.fb.com/blynkapp
+                                http://twitter.com/blynk_app
+
+  Blynk library is licensed under MIT license
+  This example code is in public domain.
+
+ *************************************************************
+
+  This example shows how to use the Menu Widget.
+
+  App project setup:
+    Menu widget attached to V1 (put 3 items in it)
+ *************************************************************/
+
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT SerialUSB
+
+
+#include <SPI.h>
+#include <WiFi101.h>
+#include <BlynkSimpleWiFiShield101.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "e03b7e93390b467b97a498e1312fa148";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "krustyt";
+char pass[] = "krabpizza";
+
+BLYNK_WRITE(V0) {
+  switch (param.asInt())
+  {
+    case 1: // Item 1
+      SerialUSB.println("Rotate right selected");
+      break;
+    case 2: // Item 2
+      SerialUSB.println("Rotate left selected");
+      break;
+     case 3: // Item 3
+      SerialUSB.println("LED on V1: on");
+      // Find way to turn on pin 7 (D7)
+      break;
+     case 4: // Item 4
+      SerialUSB.println("LED on V1: off");
+      // Find way to turn off pin 7 (D7)
+      break;
+    default:
+      SerialUSB.println("Unknown item selected");
+  }
+}
+
+void setup()
+{
+  // Debug console
+  SerialUSB.begin(9600);
+
+  Blynk.begin(auth, ssid, pass);
+  // You can also specify server:
+  //Blynk.begin(auth, ssid, pass, "blynk-cloud.com", 8442);
+  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8442);
+}
+
+void loop()
+{
+  Blynk.run();
+}
+
